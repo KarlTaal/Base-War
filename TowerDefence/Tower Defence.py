b566aa164b6574e2447a7e7eeb44e28f436459ba
KarlTaal <karltaal@gmail.com>
2018-11-10 17:41:29 +0200

    mäng läheb kinni kui tornini jõuab miski

--- TowerDefence/Tower Defence.py
+++ TowerDefence/Tower Defence.py
@@ -43,6 +43,32 @@ for mehike in mehike_walkRight:
     mehike_walkLeft.append(pygame.transform.flip(mehike, True, False))
 
 
+#vasaku torni sprite
+class vasaktorn(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((x_global * 0.06, y_global * 0.1))
+        self.image.fill((0, 0, 0))
+        self.rect = self.image.get_rect()
+        self.rect.bottomleft = (0, 0.9 * y_global)
+vasak_torn_sprite = pygame.sprite.Group()
+vasaktorn = vasaktorn()
+vasak_torn_sprite.add(vasaktorn)
+
+
+#parema torni sprite
+class paremtorn(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((x_global * 0.06, y_global * 0.1))
+        self.image.fill((0, 0, 0))
+        self.rect = self.image.get_rect()
+        self.rect.bottomright = (x_global, 0.9 * y_global)
+parem_torn_sprite = pygame.sprite.Group()
+paremtorn = paremtorn()
+parem_torn_sprite.add(paremtorn)
+
+
 class Player2(pygame.sprite.Sprite):
     def __init__(self, position, mehike_walkLeft):
         super(Player2, self).__init__()
@@ -65,14 +91,15 @@ class Player2(pygame.sprite.Sprite):
     def update(self, dt):
         self.update_time_dependent(dt)
         self.rect.x -= 5
+
 def player2():
     player2 = Player2(position=(x_global - int(0.07 * x_global) , y_global * 0.76), mehike_walkLeft=mehike_walkLeft)
     player_list2.add(player2)
 
 
-class Player(pygame.sprite.Sprite):
+class Player1(pygame.sprite.Sprite):
     def __init__(self, position, mehike_walkRight):
-        super(Player, self).__init__()
+        super(Player1, self).__init__()
         size = (int(0.07 * x_global), int(0.13 * y_global))
         self.rect = pygame.Rect(position, size)
         self.mehike_walkRight = mehike_walkRight
@@ -92,60 +119,67 @@ class Player(pygame.sprite.Sprite):
     def update(self, dt):
         self.update_time_dependent(dt)
         self.rect.x += 5
-def player():
-    player = Player(position=(0, y_global * 0.76), mehike_walkRight=mehike_walkRight)
-    player_list.add(player)
+def player1():
+    player1 = Player1(position=(0, y_global * 0.76), mehike_walkRight=mehike_walkRight)
+    player_list1.add(player1)
 
 
 def kokkupuude():
-    player_hit_list = pygame.sprite.groupcollide(player_list, player_list2, False, False, collided=None)
+    player_hit_list = pygame.sprite.groupcollide(player_list1, player_list2, False, False, collided=None)
     for i in player_hit_list:
         suvaline = random.randint(0,2)
         if suvaline == 0:
-            player_list.remove(i)
+            player_list1.remove(i)
         elif suvaline == 1:
             player_list2.remove(player_hit_list[i])
 
 
 
+
 def draw():
 
+    kokkupuude()
     mängu_screen.blit(taust, (0, 0))
-    player_list.update(dt)
+    player_list1.update(dt)
     player_list2.update(dt)
-    player_list.draw(mängu_screen)
+    player_list1.draw(mängu_screen)
     player_list2.draw(mängu_screen)
+    vasak_torn_sprite.update()
+    vasak_torn_sprite.draw(mängu_screen)
     mängu_screen.blit(vasak_torn, (0, 0.635 * y_global))
+    parem_torn_sprite.update()
+    parem_torn_sprite.draw(mängu_screen)
     mängu_screen.blit(parem_torn, (x_global - int(0.15 * y_global), 0.635 * y_global))
     pygame.display.update()
 
 
 
-
-
-
-
 player_list2 = pygame.sprite.Group()
-player_list = pygame.sprite.Group()
+player_list1 = pygame.sprite.Group()
 a = True
 while a:
     dt = clock.tick(FPS) / 1000
 
-    kokkupuude()
+    k = pygame.sprite.groupcollide(player_list2, vasak_torn_sprite, False, False, collided=None)
+    for i in k:
+        player_list2.remove(i)
+        a = False
+    k2 = pygame.sprite.groupcollide(player_list1, parem_torn_sprite, False, False, collided=None)
+    for i in k2:
+        player_list1.remove(i)
+        a = False
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             a = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_a:
-                player()
+                player1()
             if event.key == pygame.K_s:
                 player2()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 a = False
-
-
     draw()
 
 pygame.quit()
